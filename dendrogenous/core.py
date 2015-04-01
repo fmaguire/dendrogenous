b096c3d137ee729c0b19d676f7dce4f6810894b8
Finlay Maguire <finlaymaguire@gmail.com>
2015-04-01 18:15:15 +0100

    Write tests for init

--- dendrogenous/core.py
+++ dendrogenous/core.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-import dendrogenous.utils as dgutils
+import dendrogenous as dg
+from dendrogenous.settings import Settings as SettingsClass
+import dendrogenous.utils
 import os
 import io
+import re
 import pymysql
 import subprocess
 from Bio import SeqIO
@@ -32,38 +35,19 @@ class Dendrogenous():
         """
         Initialise the class and truncate the accession to 20-chars
         """
-        self.settings = settings
+        if not isinstance(settings, SettingsClass):
+            raise ValueError("Supplied settings is not Settings class")
 
-        self.seq_name = utils.reformat_accession(seq_record)
+        if not isinstance(seq_record, SeqRecord):
+            raise ValueError("Supplied sequence is not SeqRecord class")
+
+        self.settings = settings
+        self.seq_name = self.__reformat_accession(seq_record)
+
         self.seed = ">{0}\n{1}".format(self.seq_name,
                                        seq_record.seq)
 
-
         self.output_dir = self.settings.output_dir
-
-
-    def __execute_cmd(self, cmd, input_str=None):
-        """
-        Execute a command using subprocess using a string as stdin
-        and returns the stdout as a string if an input_str is provided
-        Otherwise just executes cmd normally
-        """
-        if input_str:
-            osnull = open(os.devnull, 'w')
-            proc = subprocess.Popen(cmd,
-                                    shell=True,
-                                    stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE,
-                                    stderr=osnull)
-            (stdout, stderr) = proc.communicate(input_str.encode())
-            osnull.close()
-
-        else:
-            proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-            (stdout, stderr) = proc.communicate()
-
-        return stdout.decode()
-
 
     def __reformat_accession(self, seq_record):
         """
@@ -79,142 +63,167 @@ class Dendrogenous():
         return reformatted_id
 
 
-    def __blast(self, genome):
-        """
-        Blast seed sequence against db using BLASTP
-        """
-        blast_settings = self.settings.blast_settings
 
-        blastp_path = self.settings.binary_paths['blastp'])
-        blast_cmd = "{0} -db {1} " \
-                    " -evalue {2} -max_target_seqs {3}" \
-                    " -outfmt 5".format(blastp_path,
-                                        genome,
-                                        blast_settings['e_value'],
-                                        blast_settings['num_seqs'])
+    #def __execute_cmd(self, cmd, input_str=None):
+    #    """
+    #    Execute a command using subprocess using a string as stdin
+    #    and returns the stdout as a string if an input_str is provided
+    #    Otherwise just executes cmd normally
+    #    """
+    #    if input_str:
+    #        osnull = open(os.devnull, 'w')
+    #        proc = subprocess.Popen(cmd,
+    #                                shell=True,
+    #                                stdin=subprocess.PIPE,
+    #                                stdout=subprocess.PIPE,
+    #                                stderr=osnull)
+    #        (stdout, stderr) = proc.communicate(input_str.encode())
+    #        osnull.close()
 
-        blast_output = self.__execute_cmd(blast_cmd, input_str=self.seed)
+    #    else:
+    #        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    #        (stdout, stderr) = proc.communicate()
 
-        return blast_output
-
-
-    def __parse_blast(self, blast_output):
-        """
-        Parses a blast_output files and queries the
-        MySQL DB to recover the appropriate sequences
-        """
-
-        # no get with default vals as this is a mandatory part of the
-        # settings.json - use them to connect to mysql server
-        con = pymysql.connect(**self.settings.db_config)
-        cur = con.cursor()
-
-        # Parse the xml output returns an iterator of blast record
-        # objects over the xml blastoutput file
-        blast_hits = NCBIXML.parse(io.StringIO(blast_output))
-
-        hit_id_set = set()
-        for blast_record in blast_hits:
-            for alignment in blast_record.alignments:
-                for hsp in alignment.hsps:
-                    hit_id_set.add(alignment.hit_def)
-
-        hit_records = []
-        for hit_id in hit_id_set:
-            cur.execute("SELECT sequence FROM cider "
-                        "WHERE protein_ID='{0}'".format(hit_id))
-            sequence = cur.fetchone()
-            if sequence is None:
-                with open('protein_IDs_that_dont_exist', 'a+') as fh:
-                   fh.write(str(hit_id) + '\n')
-                continue
-                #raise Exception("Failed to find"
-                #                " protein_ID in DB: {0}".format(hit_id))
-
-            sequence_record = SeqRecord(\
-                Seq(sequence[0], IUPAC.protein), id=hit_id, description='')
-            hit_records.append(sequence_record)
-        con.close()
-
-        return hit_records
+    #    return stdout.decode()
 
 
-    def get_seqs(self):
-        """
-        Get similar sequences to seed by blasting genomes and parsing the output
-        """
-        self.output_seqs = os.path.join(self.output_dir, "sequences",
-                                        self.seq_name) # file containing blast hits
-        num_hits = 0
-        for genome in self.settings['genome_list']:
-            blast_output = self._blast(genome)
-            blast_hits = self._parse_blast(blast_output)
-            num_hits += len(blast_hits)
-            SeqIO.write(blast_hits, self.output_seqs, 'fasta')
 
-        if num_hits < self.settings['minimum_seqs']:
-            os.rename(self.output_seqs,
-                      self.output_seqs+ '_too_few_seqs_for_alignment')
+    #def __blast(self, genome):
+    #    """
+    #    Blast seed sequence against db using BLASTP
+    #    """
+    #    blast_settings = self.settings.blast_settings
 
-        assert False
+    #    blastp_path = self.settings.binary_paths['blastp'])
+    #    blast_cmd = "{0} -db {1} " \
+    #                " -evalue {2} -max_target_seqs {3}" \
+    #                " -outfmt 5".format(blastp_path,
+    #                                    genome,
+    #                                    blast_settings['e_value'],
+    #                                    blast_settings['num_seqs'])
 
-    def align(self):
-        """
-        Align input seqs using kalign
-        """
+    #    blast_output = self.__execute_cmd(blast_cmd, input_str=self.seed)
 
-        if not os.path.exists(ALIGNMENT_FILE):
-            self.seqs()
-
-        align_cmd = "kalign -i {0} -o {1}".format(input_seqs,
-                                              output_aligned_seqs)
-        utils.execute_cmd(align_cmd)
-
-    def mask(self):
-        '''Mask input alignment using trimal'''
-
-        if not os.path.exists(alignment_file):
-            self.align()
-
-        mask_cmd = "trimal -in {0} -out {1} -nogaps".format(input_aligned_seqs,
-                                                            output_masked_seqs)
-        utils.execute_cmd(mask_cmd)
-
-        mask_length = accessories.mask_check(output_masked_seqs)
-
-        if mask_length < cut_off:
-            remask_cmd = "trimal -in {0}" \
-                         " -out {1} -automated1".format(input_aligned_seqs,
-                                                        output_masked_seqs)
-            utils.execute_cmd(remask_cmd)
-            new_mask_length = self.mask_check(output_masked_seqs)
-            if mask_length < cut_off:
-                mask_error = "{0} is" \
-                             " too short" \
-                             " after masking".format(output_masked_seqs)
-                os.rename(output_masked_seqs,
-                          output_masked_seqs + "_mask_too_short")
-
-                # write to sqlite db
+    #    return blast_output
 
 
-    def phylogeny(self):
-        '''Generate phylogeny from masked seqs using FastTree2'''
+    #def __parse_blast(self, blast_output):
+    #    """
+    #    Parses a blast_output files and queries the
+    #    MySQL DB to recover the appropriate sequences
+    #    """
 
-        if not os.path.exists(self.mask_file)
-            self.mask()
+    #    # no get with default vals as this is a mandatory part of the
+    #    # settings.json - use them to connect to mysql server
+    #    con = pymysql.connect(**self.settings.db_config)
+    #    cur = con.cursor()
 
-        phylogeny_cmd = "FastTree -bionj -slow" \
-                        " -quiet -out {1} {0}".format(input_masked_seqs,
-                                                      output_phylogeny)
-        utils.execute_cmd(phylogeny_cmd)
+    #    # Parse the xml output returns an iterator of blast record
+    #    # objects over the xml blastoutput file
+    #    blast_hits = NCBIXML.parse(io.StringIO(blast_output))
+
+    #    hit_id_set = set()
+    #    for blast_record in blast_hits:
+    #        for alignment in blast_record.alignments:
+    #            for hsp in alignment.hsps:
+    #                hit_id_set.add(alignment.hit_def)
+
+    #    hit_records = []
+    #    for hit_id in hit_id_set:
+    #        cur.execute("SELECT sequence FROM cider "
+    #                    "WHERE protein_ID='{0}'".format(hit_id))
+    #        sequence = cur.fetchone()
+    #        if sequence is None:
+    #            with open('protein_IDs_that_dont_exist', 'a+') as fh:
+    #               fh.write(str(hit_id) + '\n')
+    #            continue
+    #            #raise Exception("Failed to find"
+    #            #                " protein_ID in DB: {0}".format(hit_id))
+
+    #        sequence_record = SeqRecord(\
+    #            Seq(sequence[0], IUPAC.protein), id=hit_id, description='')
+    #        hit_records.append(sequence_record)
+    #    con.close()
+
+    #    return hit_records
 
 
-    def name(self):
-        """
+    #def get_seqs(self):
+    #    """
+    #    Get similar sequences to seed by blasting genomes and parsing the output
+    #    """
+    #    self.output_seqs = os.path.join(self.output_dir, "sequences",
+    #                                    self.seq_name) # file containing blast hits
+    #    num_hits = 0
+    #    for genome in self.settings['genome_list']:
+    #        blast_output = self._blast(genome)
+    #        blast_hits = self._parse_blast(blast_output)
+    #        num_hits += len(blast_hits)
+    #        SeqIO.write(blast_hits, self.output_seqs, 'fasta')
 
-        """
-        raise NotImplemented
-        #if not os.path.exists(PHYLOGENY):
-        #    self.phylogeny()
+    #    if num_hits < self.settings['minimum_seqs']:
+    #        os.rename(self.output_seqs,
+    #                  self.output_seqs+ '_too_few_seqs_for_alignment')
+
+    #    assert False
+
+    #def align(self):
+    #    """
+    #    Align input seqs using kalign
+    #    """
+
+    #    if not os.path.exists(ALIGNMENT_FILE):
+    #        self.seqs()
+
+    #    align_cmd = "kalign -i {0} -o {1}".format(input_seqs,
+    #                                          output_aligned_seqs)
+    #    utils.execute_cmd(align_cmd)
+
+    #def mask(self):
+    #    '''Mask input alignment using trimal'''
+
+    #    if not os.path.exists(alignment_file):
+    #        self.align()
+
+    #    mask_cmd = "trimal -in {0} -out {1} -nogaps".format(input_aligned_seqs,
+    #                                                        output_masked_seqs)
+    #    utils.execute_cmd(mask_cmd)
+
+    #    mask_length = accessories.mask_check(output_masked_seqs)
+
+    #    if mask_length < cut_off:
+    #        remask_cmd = "trimal -in {0}" \
+    #                     " -out {1} -automated1".format(input_aligned_seqs,
+    #                                                    output_masked_seqs)
+    #        utils.execute_cmd(remask_cmd)
+    #        new_mask_length = self.mask_check(output_masked_seqs)
+    #        if mask_length < cut_off:
+    #            mask_error = "{0} is" \
+    #                         " too short" \
+    #                         " after masking".format(output_masked_seqs)
+    #            os.rename(output_masked_seqs,
+    #                      output_masked_seqs + "_mask_too_short")
+
+    #            # write to sqlite db
+
+
+    #def phylogeny(self):
+    #    '''Generate phylogeny from masked seqs using FastTree2'''
+
+    #    if not os.path.exists(self.mask_file)
+    #        self.mask()
+
+    #    phylogeny_cmd = "FastTree -bionj -slow" \
+    #                    " -quiet -out {1} {0}".format(input_masked_seqs,
+    #                                                  output_phylogeny)
+    #    utils.execute_cmd(phylogeny_cmd)
+
+
+    #def name(self):
+    #    """
+
+    #    """
+    #    raise NotImplemented
+    #    #if not os.path.exists(PHYLOGENY):
+    #    #    self.phylogeny()
 
--- dendrogenous/test/test_core.py
+++ dendrogenous/test/test_core.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import dendrogenous
-import dendrogenous.core as core
+import dendrogenous as dg
+import dendrogenous.core
+
 import unittest
+import unittest.mock as mock
 import os
 import shutil
 import sys
@@ -28,114 +30,151 @@ class TestCore(BaseTestCase):
                    id="YP_025292.1", name="HokC",
                    description="toxic membrane protein, small")
 
-        cls.settings = {"genome_list" : ["Escherichia_coli_IAI39.fas",
-                                         "Escherichia_coli_O157_H7_str._Sakai.fas",
-                                         "Nanoarchaeum_equitans_Kin4-M.fas"],
-                        "genome_dir" : os.path.join('dendrogenous',
-                                                    'test',
-                                                    'resources'),
-                        "output_dir" : 'core_test_dir',
-                        "binary_path": os.path.join('dendrogenous',
-                                                    'dependencies'),
-                        "db_config" : {"host": "REDACTED",
-                                       "db": "new_proteins",
-                                       "user": "orchard",
-                                       "passwd": "REDACTED"},
-                        "minimum_seqs": 3}
+        #cls.settings = {"genome_list" : ["Escherichia_coli_IAI39.fas",
+        #                                 "Escherichia_coli_O157_H7_str._Sakai.fas",
+        #                                 "Nanoarchaeum_equitans_Kin4-M.fas"],
+        #                "genome_dir" : os.path.join('dendrogenous',
+        #                                            'test',
+        #                                            'resources'),
+        #                "output_dir" : 'core_test_dir',
+        #                "binary_path": os.path.join('dendrogenous',
+        #                                            'dependencies'),
+        #                "db_config" : {"host": "REDACTED",
+        #                               "db": "new_proteins",
+        #                               "user": "orchard",
+        #                               "passwd": "REDACTED"},
+        #                "minimum_seqs": 3}
 
-        output_dir = cls.settings['output_dir']
-        subdirs = ['sequences',
-                   'alignments',
-                   'masks',
-                   'phylogenies', os.path.join('phylogenies', 'coded_named')]
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
-            for subdir in subdirs:
-                os.mkdir(os.path.join(output_dir, subdir))
+        #output_dir = cls.settings['output_dir']
+        #subdirs = ['sequences',
+        #           'alignments',
+        #           'masks',
+        #           'phylogenies', os.path.join('phylogenies', 'coded_named')]
+        #if not os.path.exists(output_dir):
+        #    os.mkdir(output_dir)
+        #    for subdir in subdirs:
+        #        os.mkdir(os.path.join(output_dir, subdir))
 
     def setUp(self):
-         self.test = core.dendrogenous(self.test_record,
-                                       self.settings)
+        #self.test_class = dg.core.Dendrogenous(self.test_record,
+        #                                       self.settings)
+        self.test_record = SeqRecord(\
+                   Seq("MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEGGEEEEVAVF",
+                   IUPAC.protein),
+                   id="YP_025292.1", name="HokC",
+                   description="toxic membrane protein, small")
+
+
 
 
     def test_init_clean(self):
         """
-        Ensure class init works correctly when there is no pre-existing output
+        Ensure class init works when provided a seqrecord and settings file
         """
+        settings = mock.Mock(dg.settings.Settings)
+        settings.output_dir = "testdir"
+
+        test_class = dg.core.Dendrogenous(self.test_record,
+                                          settings)
+
+        expected_name = "YP_025292_1"
         expected_seed = (">YP_025292_1\n"
                          "MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEGGEEEEVAVF")
 
-        self.assertEqual(self.test.seed, expected_seed)
-        self.assertIs(self.test.state, False)
+        self.assertEqual(test_class.seed, expected_seed)
+        self.assertEqual(test_class.seq_name, expected_name)
+        self.assertEqual(test_class.output_dir, settings.output_dir)
 
 
-    def test__blast(self):
+    def test_init_bad_seqrecord(self):
         """
-        Test blast function returns output
+        Ensure ValueError is raised if class is instantiated without valid seqrecord
         """
-        blast_output = self.test._blast("Escherichia_coli_O157_H7_str._Sakai.fas")
+        settings = mock.Mock(dg.settings.Settings)
+        settings.output_dir = "testdir"
 
-        expected_output = self.parse_file(os.path.join(self.test_resources,
-                                                       "expected_core_blastp_output.xml"))
+        invalid_seq = ""
 
-        self.assertEqual(blast_output.split(os.linesep), expected_output)
+        with self.assertRaises(ValueError):
+            dg.core.Dendrogenous(invalid_seq,
+                                 settings)
 
-
-    @unittest.skip('tests local server')
-    def test__parse(self):
+    def test_init_bad_settings(self):
         """
-        Ensure correct parsing of hits locally when connected to server
+        Ensure ValueError is raised if class is instantiated without valid seqrecord
         """
+        invalid_settings = ""
 
-        blastp_xml = self.parse_file(os.path.join(self.test_resources,
-                                                  "expected_core_blastp_output.xml"))
-
-        blastp_output = "\n".join(blastp_xml)
-
-        parsed_blast = self.test._parse_blast(blastp_output)
-
-        expected_parsed_hit_id = '15829270'
-
-        self.assertEqual(parsed_blast[0].id, expected_parsed_hit_id)
+        with self.assertRaises(ValueError):
+            dg.core.Dendrogenous(self.test_record,
+                                 invalid_settings)
 
 
-    def test_get_seqs(self):
-        """
-        Test dendrogenous.get_seqs() works correctly and writes a file of seqs
-        to the appropriate directory
-        Ensure state is correctly updated
-        """
-        #self.test.get_seqs()
-        ##for some reason I can't even inspect these
-        #self.fail()
-        pass
+    #def test__blast(self):
+    #    """
+    #    Test blast function returns output
+    #    """
+    #    blast_output = self.test._blast("Escherichia_coli_O157_H7_str._Sakai.fas")
+
+    #    expected_output = self.parse_file(os.path.join(self.test_resources,
+    #                                                   "expected_core_blastp_output.xml"))
+
+    #    self.assertEqual(blast_output.split(os.linesep), expected_output)
 
 
-    def test_get_seqs_insufficient(self):
-        """
-        Test that dendrogenous.get_seqs() correctly renames parsed blast hit
-        output file if there are not sufficient numbers of sequences
-        Ensure state is correctly updated
-        """
-        pass
-        #self.fail()
+    #@unittest.skip('tests local server')
+    #def test__parse(self):
+    #    """
+    #    Ensure correct parsing of hits locally when connected to server
+    #    """
 
-    def test_align(self):
-        """
+    #    blastp_xml = self.parse_file(os.path.join(self.test_resources,
+    #                                              "expected_core_blastp_output.xml"))
 
-        """
-        pass
-        #self.fail()
+    #    blastp_output = "\n".join(blastp_xml)
+
+    #    parsed_blast = self.test._parse_blast(blastp_output)
+
+    #    expected_parsed_hit_id = '15829270'
+
+    #    self.assertEqual(parsed_blast[0].id, expected_parsed_hit_id)
+
+    #def test_get_seqs(self):
+    #    """
+    #    Test dendrogenous.get_seqs() works correctly and writes a file of seqs
+    #    to the appropriate directory
+    #    Ensure state is correctly updated
+    #    """
+    #    #self.test.get_seqs()
+    #    ##for some reason I can't even inspect these
+    #    #self.fail()
+
+
+    #def test_get_seqs_insufficient(self):
+    #    """
+    #    Test that dendrogenous.get_seqs() correctly renames parsed blast hit
+    #    output file if there are not sufficient numbers of sequences
+    #    Ensure state is correctly updated
+    #    """
+    #    pass
+    #    #self.fail()
+
+    #def test_align(self):
+    #    """
+
+    #    """
+    #    pass
+    #    #self.fail()
 
 
 
-    @classmethod
-    def tearDownClass(cls):
-        """
-        Clean up the outputs generated during testing
-        """
-        if os.path.exists(cls.settings['output_dir']):
-            shutil.rmtree(cls.settings['output_dir'])
+    #@classmethod
+    #def tearDownClass(cls):
+    #    """
+    #    Clean up the outputs generated during testing
+    #    """
+    #    if os.path.exists(cls.settings['output_dir']):
+    #        shutil.rmtree(cls.settings['output_dir'])
 
 if __name__ == '__main__':
     unittest.main()
